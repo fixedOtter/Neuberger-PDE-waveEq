fa8a3706673b04507e229c3eaec5f6fc473c9a28
fixedotter <gj392@nau.edu>
2026-05-03 22:30:21 -0700

    updated readme; working on plotting neuberger eq on cube

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# Neuberger-PDE-waveEq
-Using John M. Neuberger's method of solving the wave equation on a 1x1 square.
+# Neuberger-PDE
+Solving a few PDE pretty closely following John M. Neuberger's Differences Matricies for ODE and PDE text.
+
+## WaveEq.m (Wave Equation)
+This script solves and plots a little movie of a 1x1 square. There's a few equations in the code to create plots like these: (so pretty!)
+![Fluctuating sin plot with damping](./img/wave-damping-final.gif)
+![Fluctuating square hammer plot](./img/taylor_wave_60.gif)
+
+## Newtons Method scripts
+Using Newtons method to solve what is essentially 
--- newtons_meth_3d.m
+++ newtons_meth_3d.m
@@ -1,8 +1,16 @@
 % newtons method for PDE's
 % written by fixedotter
 
+clear; clc;
+
 % boring const
-n = 10; dx = 1/n; ds = 0.1;
+n = 20; dx = 1/n; n_half = round(n/2);
+ds = 0.1; % s_f = 3*pi^2; % final s
+s_f = 0;
+
+% array of s 
+s_array = 0:ds:s_f;
+u_array = zeros(1,length(s_array)); % to store solutions for each s
 
 % basically just the grid
 x = linspace(0,1,n+1)'; xs = x(2:end-1); % full and interior pts in interval 
@@ -13,22 +21,25 @@ I  = speye(n-1);
 I_sq = speye((n-1)^2);
 D2 = spdiags(kron(ones(n-1,1),[1 -2 1]),-1:1,n-1,n-1)/dx^2;
 
-% Laplacian = D_2^x + D_2^y
+% Laplacian = D_2x + D_2^y
 L  = kron(I,D2) + kron(D2, I);
 
 % laplacian w/ cube
 L_cub = kron(I_sq,D2) + kron(L,I);
 
 % compute first 6 eigen pairs
-[V,D] = eigs(L_cub,6,'sm'); 
-% diag(D)
+[V,D] = eigs(-L_cub,6,'sm'); 
+disp(D)
 % 6 smallest DIFFERENT eigenvalues are 3*pi^2, 6*pi^2, 9*pi^2, 11*pi^2, 12*pi^2, 14*pi^2
 % since there's a linear combination, we basically get 3 of each of them after 3*pi^2, which is what we see in the diag(D) output
 
 % function & it's jacobian with s to move along solution space?
 F = @(s,u) L_cub*u + s*u + u.^3; % actual funct with s to move along the solutions
 J = @(s,u) L_cub + spdiags(s + 3*u.^2, 0, (n-1)^3, (n-1)^3); % jacobian with s
-f = @(x,y,z) sin(pi*x).*sin(pi*y).*sin(pi*z)*4; % init guess
+
+% for some reason 8 times the whole thing is a good guess but 9 or 7 isnt
+f = @(x,y,z) sin(pi*x).*sin(pi*y).*sin(pi*z)*8; % init guess
+
 
 % putting her into a column for easy matrix ops
 u = reshape(f(XS,YS,ZS), (n-1)^3, 1);
@@ -42,36 +53,74 @@ u = reshape(f(XS,YS,ZS), (n-1)^3, 1);
 %   end
 % end
 
-% just doing s = 1 for now
-s = 1;
-chi = J(s,u)\F(s,u); % next newton step
-u = u - chi; % update
-% if (norm(chi) < 1e-6) % stopping condition
-%   disp("converged")
-% else
-%   disp("not converged")
-% end
+% just doing s = 0 for now
+for s = 0:ds:s_f
+  % newtons meth step
+  for i = 1:250
+    chi = J(s,u)\F(s,u); % next newton step
+    u = u - chi; % update
+    fprintf("iteration %d, norm of chi: %e\n", i, norm(chi));
+    disp(max(abs(u))); % max value of u
+    if (norm(chi) < 1e-6) % stopping condition
+      break
+    end
+  end
+  u_array(s+1) = max(abs(u)); % store the max value of u for this s
+end
 
 % bringing it back to 3d for plotting
 u = reshape(u, n-1, n-1, n-1); % reshape function
-Z = zeros(n+1,n+1,n+1); % boundary conditions
-Z(2:end-1,2:end-1,2:end-1) = u; % feeding in the solution
+Sol_3d = zeros(n+1,n+1,n+1); % boundary conditions
+Sol_3d(2:end-1,2:end-1,2:end-1) = u; % feeding in the solution
 [X,Y,Z] = meshgrid(x,x,x); % grid for plotting
+
 
 % can we save this to a mf gif
 gifFile = "neubergers_cube_w_newtonsMeth.gif";
-for k = 0:n
-  figure
-  colormap(jet); % shading interp
-  Z_slice = Z(:,:,k+1); % slicing the solution at z = k*ds
-  zlim([-1,1]); % keeping the z axis fixed so it's less confusing
-  surf(X(:,:,k+1),Y(:,:,k+1),Z_slice,'edgecolor','none'); % plotting the slice of the solution
-  titleName = strcat('z = ', num2str(k*dx));
-  title(titleName)
-  view(42,-42);
-  % exportgraphics(gcf, strcat('neubergers_cube_w_newtonsMeth_s=', num2str(k*ds), '.png')); % saving the figure
-  exportgraphics(gcf,gifFile,Append=true); % saving to the gif
-end
+% for k = 0:n
+%   figure("Visible",'off')
+%   colormap(jet); % shading interp
+%   Z_slice = Z(:,:,k+1); % slicing the solution at z = k*ds
+%   zlim([-1,1]); % keeping the z axis fixed so it's less confusing
+%   xlim([0,1]); ylim([0,1]); % keeping x and y axis fixed too
+%   surf(X(:,:,k+1),Y(:,:,k+1),Z_slice,'edgecolor','none'); % plotting the slice of the solution
+%   titleName = strcat('z = ', num2str(k*dx));
+%   title(titleName)
+%   view(42,-42);
+%   % exportgraphics(gcf, strcat('neubergers_cube_w_newtonsMeth_s=', num2str(k*ds), '.png')); % saving the figure
+%   exportgraphics(gcf,gifFile,Append=true); % saving to the gif
+% end
+
+% surface figure
+figure
+surf(X(:,:,n_half),Y(:,:,n_half),Sol_3d(:,:,n_half),'edgecolor','none'); % plotting the slice of the solution
+title('Slice of the solution at z = 0.5')
+% zlim([0 10E-34])
+view(42,-42);
+
+scatX = reshape(X, [],1,1);
+scatY = reshape(Y, [],1,1);
+scatZ = reshape(Z, [],1,1);
+scatSol_3d = reshape(Sol_3d, [],1,1);
+
+% scatter3d figure
+figure
+scatter3(scatX,scatY,scatZ)
+colormap(gca,"parula")
+
+figure
+plot(s_array, u_array, '-o');
+xlabel('s value');
+ylabel('max absolute value u');
+title('max |u| vs s');
+grid on;
+
+% figure
+% colormap(jet); % shading interp
+% isosurface(X,Y,Z,0); % plotting the isosurface of the solution
+% title('Isosurface of the solution at s = 3*pi^2')
+% view(42,-42);
+
 
 
   % what chat said
